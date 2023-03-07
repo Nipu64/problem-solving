1c385cd5e35b3f3ce7d3d3919fd28551b693ae4c
Mst.Hasna Noushin <70809195+Nipu64@users.noreply.github.com>
2023-03-08 00:41:36 +0600

    Add files via upload

---
+++ String/substring.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define sync ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+using namespace std;
+int main()
+{
+    //sync;
+    string s;
+    cin>>s;
+    string ss;
+    ss= s.substr(2,5);
+    cout<<ss<<"\n";
+
+    return 0;
+}
