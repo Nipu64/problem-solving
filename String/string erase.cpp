e57061526c22b555550ac11f5dbb9b5f4830e78c
Mst.Hasna Noushin <70809195+Nipu64@users.noreply.github.com>
2023-03-08 00:45:47 +0600

    Add files via upload

---
+++ String/string erase.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define sync ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+using namespace std;
+int main()
+{
+    //sync;
+    string s;
+    cin>>s;
+    s.erase(s.begin()+0);
+    cout<<s<<"\n";
+    return 0;
+}
