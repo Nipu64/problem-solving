0ab8a034955ff265e655626d68892dbaabc03215
Mst.Hasna Noushin <70809195+Nipu64@users.noreply.github.com>
2023-03-10 19:54:08 +0600

    Add files via upload

---
+++ Number Theory/GCD.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define sync ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+using namespace std;
+
+ll gcd(ll a,ll b){
+if(b==0)
+    return a;
+else
+    return gcd(b,a%b);       //o(log(min(a,b)))
+}
+
+int main()
+{
+    //sync;
+    ll a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b)<<"\n";
+    return 0;
+}
