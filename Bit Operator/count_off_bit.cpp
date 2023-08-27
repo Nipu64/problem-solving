0cce88468a13e22e1b022c5d9cb80df2df899ee5
Mst.Hasna Noushin <70809195+Nipu64@users.noreply.github.com>
2023-08-27 07:24:07 +0600

    Add files via upload

---
+++ Bit Operator/count_off_bit.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool cheak_kth_bits(int x,int k)
+{
+    return (x>>k)&1;
+}
+int print_on_bits(int x)
+{
+    int cnt=0;
+    for(int i=0;i<32;i++)
+    {
+        if(!cheak_kth_bits(x,i))
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<print_on_bits(x);
+}
