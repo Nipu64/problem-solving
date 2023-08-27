a7501f849732d63d6ad71a4a57674967055d19ca
Mst.Hasna Noushin <70809195+Nipu64@users.noreply.github.com>
2023-08-27 06:37:21 +0600

    Update on_k_th_bit.cpp

--- Bit Operator/on_k_th_bit.cpp
+++ Bit Operator/on_k_th_bit.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int on_kth_bit(int x,int k)
+  {
+    return x|(1<<k);//1 0 1 1
+                    //0 1 0 0->1 k=2 ghor right shiting mean  3 ghor
+                    //----------
+                    //1 1 1 1->15
+  }
+int main()
+{
+    
+    cout<<on_kth_bit(11,2);
 
+}
